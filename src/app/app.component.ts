c5d87695d478af8e2eabce70202c48161aa06383
Esteban Canaza Ayarachi <ecanaza232@gmail.com>
2024-10-11 00:19:03 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { DarkModeService } from './services/dark-mode.service';
+import { CommonModule } from '@angular/common';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-material-talwind';
+  darkModeService: DarkModeService = inject(DarkModeService);
+  toggleDarkMode() {
+    this.darkModeService.updateDarkMode();
+  }
 }
---
+++ src/app/services/dark-mode.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DarkModeService {
+darkModeSignal = signal<string>('null');
+  constructor() { }
+
+  updateDarkMode() {
+    this.darkModeSignal.update((value) => ( value === 'dark' ? 'null' : 'dark'));
+  }
+}
